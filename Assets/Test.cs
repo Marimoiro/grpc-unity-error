0bb3dfbba8afd8a0107fb8b0687d9be7f31e7144
Marimoiro <momiziiro@gmail.com>
2019-03-20 19:29:37 +0900

    linker error project

---
+++ Assets/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Grpc.Core;
+using Helloworld;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+	    Greeter.GreeterClient client = new Greeter.GreeterClient(new Channel("localhost", 12345,ChannelCredentials.Insecure));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
